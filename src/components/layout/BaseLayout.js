55e5bf1925c949e1da2e54f18f259480699b62e0
Dan Gelok <dgelok@gmail.com>
2020-09-09 18:12:05 -0500

    some sidebar visibility

--- src/components/layout/BaseLayout.js
+++ src/components/layout/BaseLayout.js
@@ -5,13 +5,16 @@ import {Col, Row, Container} from 'react-bootstrap'
 import '../style/sidebar.css'
 
 const BaseLayout = (props) => {
+    
+    let auth = true 
+    let visibility = auth ? 'visible' : 'hidden'
   return (
     <>
     
         <Header />
     <Container className="m-0">   
         <Row style={ { height: 100 } }>
-            <Col className="col-2 bg-warning sidebar">
+            <Col className="col-2 bg-warning sidebar" style={{visibility:visibility}}>
                 <Sidebar />
             </Col>
             <Col>
