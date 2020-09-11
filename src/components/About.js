906e734791a234db010b8535adb7a0c53395ec4c
Dan Gelok <dgelok@gmail.com>
2020-09-11 11:09:40 -0500

    visual updates

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ import guy from './images/no.png'
 import woody from './images/woodyheadshot.jpg'
 import jeremy from './images/jeremy.jpg'
 import chris from './images/chris.jpg'
+import dan from './images/Dan.png'
 
 const About = () => {
   return (
@@ -18,8 +19,8 @@ const About = () => {
                     <Card.Text>
                     Role: Back-end Development, Architecture
                     </Card.Text>
-                    {/* secondary link: "https://www.linkedin.com/in/jeremyroussel/" */}
-                    <Button variant="primary" href="https://github.com/JeremyRoussel/" target="_blank">Github</Button>
+                    <a href="https://www.linkedin.com/in/jeremyroussel/" target="_blank"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a>&nbsp;&nbsp;
+                    <a href="https://github.com/JeremyRoussel" target="_blank"><img src="https://img.shields.io/badge/GitHub-%23181717.svg?&style=for-the-badge&logo=github&logoColor=white"/></a>
                 </Card.Body>
             </Card>
         
@@ -31,22 +32,25 @@ const About = () => {
                     <Card.Text>
                     Role: Back-end Development, API handling
                     </Card.Text>
-                    {/* secondary link: "https://www.linkedin.com/in/woody-connell/" */}
-                    <Button variant="primary" href="https://github.com/woody-connell/" target="_blank">Github</Button>
+                    
+                    <a href="https://www.linkedin.com/in/woody-connell/" target="_blank"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a>&nbsp;&nbsp;
+                    <a href="https://github.com/woody-connell/" target="_blank"><img src="https://img.shields.io/badge/GitHub-%23181717.svg?&style=for-the-badge&logo=github&logoColor=white"/></a>
                 </Card.Body>
             </Card>
         </Row>
         <Row className="d-flex justify-content-center mt-5">
 
             <Card style={{ width: '15rem' }} className="m-1">
-                <Card.Img variant="top" src={guy} />
+                <Card.Img variant="top" src={dan} />
                 <Card.Body>
                     <Card.Title>Dan Gelok</Card.Title>
                     <Card.Text>
                     Role: Front-end Development, UX
                     </Card.Text>
-                    {/* secondary link: "https://www.linkedin.com/in/dangelok/" */}
-                    <Button variant="primary" href="https://github.com/dgelok/" target="_blank">Github</Button>
+                    
+                    <a href="https://www.linkedin.com/in/dangelok/" target="_blank"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a>&nbsp;&nbsp;
+                    <a href="https://github.com/dgelok/" target="_blank"><img src="https://img.shields.io/badge/GitHub-%23181717.svg?&style=for-the-badge&logo=github&logoColor=white"/></a>
+                    
                 </Card.Body>
             </Card>
             <Card style={{ width: '15rem' }} className="m-1">
@@ -56,7 +60,9 @@ const About = () => {
                     <Card.Text>
                     Role: Front-end Development, animations
                     </Card.Text>
-                    <Button variant="primary" href="https://github.com/CM-David/" target="_blank">Github</Button>
+                    <a href="https://www.linkedin.com/in/chris-m-david/" target="_blank"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a>&nbsp;&nbsp;
+                    <a href="https://github.com/CM-David/" target="_blank"><img src="https://img.shields.io/badge/GitHub-%23181717.svg?&style=for-the-badge&logo=github&logoColor=white"/></a>
+                    
                 </Card.Body>
             </Card>
         </Row>
