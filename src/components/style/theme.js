125092fb4f4fe4a8a0c628c39c9b30446dc787f3
CM-David <CM.David88@yahoo.com>
2020-09-14 15:54:29 -0500

    color pallete

--- src/components/style/theme.js
+++ src/components/style/theme.js
@@ -1,7 +1,19 @@
+
+// emerald green #50c878
+// Dark sky blue #93c6d6
+// White #fcfffc
+// Mountbatten Pink #93748a
+// Charleston Green #2e3532
+
+
 export const theme = {
-    primaryDark: '#0D0C1D',
-    primaryLight: '#EFFFFA',
-    primaryHover: '#343078',
+    primaryDark: '#2e3532',
+    primaryLight: '#50C878',
+    primaryHover: '#93748a',
     mobile: '900px',
     mobile2: '576px',
 };
+
+
+
+
