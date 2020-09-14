14c4ad8887e98e2abdafbdf7a41548f561e1980f
Dan Gelok <dgelok@gmail.com>
2020-09-14 12:07:10 -0500

    added modal to compose

--- src/components/Compose.js
+++ src/components/Compose.js
@@ -1,13 +1,17 @@
 import React, {useState} from 'react'
 import requireAuth from '../requireAuth'
 import CKEditor from 'ckeditor4-react';
-
+import {Button, Modal} from 'react-bootstrap'
 
 const Compose = () => {
 
   let [editorData, updateEditorData] = useState("Hello from CKeditor!")
+  const [show, setShow] = useState(false);
+  const modalShow = () => setShow(true);
+  const modalClose = () => setShow(false);
 
   let handleSubmit = () =>{
+      modalShow()
       console.log(editorData)
   }
 
@@ -16,6 +20,7 @@ const Compose = () => {
   }
 
   return (
+    <>
     <div className="App m-5">
       <CKEditor
           data={editorData} 
@@ -23,6 +28,22 @@ const Compose = () => {
       />
       <button type="button" onClick={handleSubmit}>Submit</button>
     </div>
+
+    <Modal show={show} onHide={modalClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Send Mail</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Which list would you like to send to?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={modalClose}>
+          Send
+        </Button>
+        <Button variant="primary" onClick={modalClose}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </>
   )
 }
 
