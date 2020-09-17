683bdf6d026f4523c95ba7c146e0e29d90669538
CM-David <CM.David88@yahoo.com>
2020-09-17 15:20:46 -0500

    splash page and font

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,18 +1,77 @@
 import React from 'react'
+import "./welcome.css"
 
 
 export default () => {
     return (
-        <div className="m-5 col-6">
-            <h1>Welcome to Mega Mail</h1>
-            <br></br>
-            <p>The best way to keep in touch with your subcribers</p>
-            <p>Images, descriptions, etc.</p>
-            <p>Plus a link to signup.</p>
-            
-
-            
-
+        <>
+        <header className="masthead text-center w-100">
+        <div className="masthead-content">
+          <div className="container">
+            <h1 className="masthead-heading mb-0">Mega Mail</h1>
+            <h2 className="masthead-subheading mb-0">Your one stop to connect with your customers</h2>
+            <a href="#" className="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+          </div>
         </div>
+        <div className="bg-circle-1 bg-circle"></div>
+        <div className="bg-circle-2 bg-circle"></div>
+        <div className="bg-circle-3 bg-circle"></div>
+        <div className="bg-circle-4 bg-circle"></div>
+      </header>
+    
+      <section>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
+                <img className="img-fluid rounded-circle" src="" alt="Image here"></img>
+              </div>
+            </div>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">For those about to mail</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <section>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="p-5">
+                <img className="img-fluid rounded-circle" src="" alt="Image here"></img>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="p-5">
+                <h2 className="display-4">We salute you!</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <section>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 order-lg-2">
+              <div className="p-5">
+                <img className="img-fluid rounded-circle" src="" alt="Image here"></img>
+              </div>
+            </div>
+            <div className="col-lg-6 order-lg-1">
+              <div className="p-5">
+                <h2 className="display-4">Mail all the things</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      </>
     )
 }
