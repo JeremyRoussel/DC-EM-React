ccd14d4d7e900b75dbe6d68efc70a9f6dbecdf5f
Dan Gelok <dgelok@gmail.com>
2020-09-20 20:02:18 -0500

    update view height for sidebar

--- src/components/style/Sidebar.styled.js
+++ src/components/style/Sidebar.styled.js
@@ -6,7 +6,7 @@ export const StyledMenu = styled.nav`
     justify-content: center;
     background: ${({ theme }) => theme.primaryLight};
     transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
-    height: 120vh;
+    height: 100vh;
     text-align: top;
     padding: 1rem;
     position: absolute;
